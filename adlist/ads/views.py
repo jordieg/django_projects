534b94f4f12cfa5d30a48b9339efd18fa01cc512
Jordan Graves <jorgrav@umich.edu>
2019-04-19 05:15:04 +0000

    stuff

--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,3 +1,132 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from ads.models import Ad, Comment
+from django.views import View
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ads.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from django.http import HttpResponse
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from ads.forms import CreateForm, CommentForm
 
-# Create your views here.
+
+class ThingListView(OwnerListView):
+    model = Ad
+    template_name = "ad_list.html"
+
+    def get(self, request) :
+        thing_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}]  (A list of rows)
+            rows = request.user.favorite_things.values('id')
+            favorites = [ row['id'] for row in rows ]
+        ctx = {'thing_list' : thing_list, 'ads': ads}
+        return render(request, self.template_name, ctx)
+
+#@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Add PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        fav = Fav(user=request.user, thing=t)
+        try:
+            fav.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+#@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Delete PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        try:
+            fav = Fav.objects.get(user=request.user, thing=t).delete()
+        except Fav.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
+
+
+class AdListView(OwnerListView):
+    model = Ad
+    template_name = "ad_list.html"
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+    template_name = "ad_detail.html"
+
+    def get(self, request, pk) :
+        ad = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ['title', 'text', 'price', 'picture']
+    template_name = "ad_form.html"
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ['title', 'text', 'price', 'picture']
+    template_name = "ad_form.html"
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
+    template_name = "ad_delete.html"
+
+class CommentCreateView(OwnerCreateView):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment_form = CommentForm(request.POST)
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse_lazy('ad_detail', args=[pk]))
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "comment_delete.html"
+
+
+
+class PicFormView(LoginRequiredMixin, View):
+    template = 'ad_form.html'
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk=None) :
+        if not pk :
+            form = CreateForm()
+        else:
+            pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(instance=pic)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        if not pk:
+            form = CreateForm(request.POST, request.FILES or None)
+        else:
+            pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Adjust the model owner before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
+
+def stream_file(request, pk) :
+    pic = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
+
